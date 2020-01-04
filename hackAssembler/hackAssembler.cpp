6170294d190af14412962297c017146ba23215f3
enricoKoschel <enricokoschel@outlook.de>
2020-01-04 22:45:17 +0100

    implemented handling of command line arguments and opening input and output files

--- hackAssembler/hackAssembler.cpp
+++ hackAssembler/hackAssembler.cpp
@@ -4,10 +4,48 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-	cout << "argc: " << argc << endl;
+string inputFileName;
+string outputFileName;
 
-	for (int i = 0; i < argc; i++) {
-		cout << "argv["
+int handleArguments(int argc, char* argv[]) {
+	if (argc == 3) {
+		if (strcmp(argv[1], "-i") == 0) {
+			inputFileName = argv[2];
+			int pos = inputFileName.find(".asm", 1);
+			if (pos != string::npos) {
+				outputFileName = inputFileName.substr(0, pos) + ".hack";
+			}
+			else {
+				return 1;
+			}
+		}
+		else {
+			return 1;
+		}
 	}
+	else {
+		return 1;
+	}
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	if (handleArguments(argc, argv) == 1) {
+		cerr << "Usage: hackAssembler.exe -i [inputFileName.asm]\noutput filename is inputFileName.hack";
+		return 1;
+	}
+
+	ifstream inputFile(inputFileName);
+	ofstream outputFile(outputFileName, ios::trunc);
+
+	if (!inputFile.is_open()) {
+		cerr << inputFileName << " could not be opened!";
+		return 1;
+	}
+	if (!outputFile.is_open()) {
+		cerr << outputFileName << " could not be created/opened!";
+		return 1;
+	}
+
+	return 0;
 }
